3aa29115a531a996b4b5b88b22b2574dcd967387
Abdelrahman <aboeittaa23@mail.wlu.edu>
2020-11-03 17:32:41 -0500

    a quick comment

--- src/picasso/parser/tokens/multiargument/PerlinColorToken.java
+++ src/picasso/parser/tokens/multiargument/PerlinColorToken.java
@@ -1,6 +1,12 @@
 package picasso.parser.tokens.multiargument;
 
+/**
+ * 
+ * PerlinColorToken
+ *
+ */
 public class PerlinColorToken {
+	
 
 	public PerlinColorToken() {
 		// TODO Auto-generated constructor stub
