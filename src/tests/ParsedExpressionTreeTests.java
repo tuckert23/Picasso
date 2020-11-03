ec501d0770c94610e6fecf9e92391291f558cfc3
Abdelrahman <aboeittaa23@mail.wlu.edu>
2020-11-02 20:35:06 -0500

    fixed

--- src/tests/ParsedExpressionTreeTests.java
+++ src/tests/ParsedExpressionTreeTests.java
@@ -38,19 +38,7 @@ public class ParsedExpressionTreeTests {
 		assertEquals(new X(), e);
 	}
 
-<<<<<<< HEAD
-	@Test
-	public void PlusExpressionTests() {
-		ExpressionTreeNode e = parser.makeExpression("x + y");
-		assertEquals(new Plus(new X(), new Y()), e);
-		
-		e = parser.makeExpression("[1,.3,-1] + y");
-		assertEquals(new Plus(new RGBColor(1, .3, -1), new Y()), e);
-		
-		e = parser.makeExpression("x + y + [ -.51, 0, 1]");
-		assertEquals(new Plus(new Plus(new X(), new Y()), new RGBColor(-.51, 0, 1)), e);
-	}
-=======
+
 //	@Test
 //	public void additionExpressionTests() {
 //		ExpressionTreeNode e = parser.makeExpression("x + y");
@@ -63,17 +51,19 @@ public class ParsedExpressionTreeTests {
 //		assertEquals(new Addition(new Addition(new X(), new Y()), new RGBColor(-.51, 0, 1)), e);
 //	}
 
-//	@Test
-//	public void parenthesesExpressionTests() {
-//		ExpressionTreeNode e = parser.makeExpression("( x + y )");
-//		assertEquals(new Addition(new X(), new Y()), e);
-//
-//		e = parser.makeExpression("( x + (y + [ 1, 1, 1] ) )");
-//		assertEquals(new Addition(new X(), new Addition(new Y(), new RGBColor(1, 1, 1))), e);
-//	}
->>>>>>> branch 'main' of https://github.com/WLU-CSCI209-F20/picasso-buildingblocks.git
 
-<<<<<<< HEAD
+	@Test
+	public void PlusExpressionTests() {
+		ExpressionTreeNode e = parser.makeExpression("x + y");
+		assertEquals(new Plus(new X(), new Y()), e);
+		
+		e = parser.makeExpression("[1,.3,-1] + y");
+		assertEquals(new Plus(new RGBColor(1, .3, -1), new Y()), e);
+		
+		e = parser.makeExpression("x + y + [ -.51, 0, 1]");
+		assertEquals(new Plus(new Plus(new X(), new Y()), new RGBColor(-.51, 0, 1)), e);
+	}
+
 	@Test
 	public void parenthesesExpressionTests() {
 		ExpressionTreeNode e = parser.makeExpression("( x + y )");
@@ -91,15 +81,6 @@ public class ParsedExpressionTreeTests {
 		e = parser.makeExpression("floor( x + y )");
 		assertEquals(new Floor(new Plus(new X(), new Y())), e);
 	}
-=======
-//	@Test
-//	public void floorFunctionTests() {
-//		ExpressionTreeNode e = parser.makeExpression("floor( x )");
-//		assertEquals(new Floor(new X()), e);
-//
-//		e = parser.makeExpression("floor( x + y )");
-//		assertEquals(new Floor(new Addition(new X(), new Y())), e);
-//	}
->>>>>>> branch 'main' of https://github.com/WLU-CSCI209-F20/picasso-buildingblocks.git
+
 
 }
