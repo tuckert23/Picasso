5e6c92a74c35bb29b4aa8eaed45901a9ec6efd5d
August Donovan <donovana22@mail.wlu.edu>
2020-11-07 22:11:41 -0500

    Initial implementation of arctangent.

---
+++ src/picasso/parser/language/expressions/unaryFunctions/Atan.java
@@ -0,0 +1,61 @@
+package picasso.parser.language.expressions.unaryFunctions;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.RGBColor;
+import picasso.parser.language.expressions.UnaryFunction;
+
+/**
+ * Represents the cosine function in the Picasso language.
+ * 
+ * @author August Donovan
+ * Based on Abdelrahman's structure in Sin
+ *
+ */
+public class Cos extends UnaryFunction {
+
+	/**
+	 * Create a cos expression that takes as a parameter the given expression
+	 * 
+	 * @param param the expression to sin
+	 */
+	
+	public Atan(ExpressionTreeNode param) {
+		super(param);
+	}
+
+	/**
+	 * Evaluates this expression at the given x,y point by evaluating the arctangent of the
+	 * function's parameter.
+	 * 
+	 * @return the color from evaluating the arctangent of the expression's parameter
+	 */
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor result = param.evaluate(x, y);
+		double red = Math.atan(result.getRed());
+		double green = Math.atan(result.getGreen());
+		double blue = Math.atan(result.getBlue());
+
+		return new RGBColor(red, green, blue);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		
+		if (!(obj instanceof Atan)) {
+			return false;
+		}
+		
+		Atan s = (Atan) obj;
+		return param.equals(s.param);
+	}
+
+}
