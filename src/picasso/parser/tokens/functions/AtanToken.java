3ae6083af60c4a541498cf7c465328588fff6b1c
August Donovan <donovana22@mail.wlu.edu>
2020-11-07 22:17:05 -0500

    Initial implementation of the arctangent token.

---
+++ src/picasso/parser/tokens/functions/AtanToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the atan function token
+ * @author August Donovan
+ *
+ */
+public class AtanToken extends FunctionToken {
+
+	public AtanToken() {
+		super("Atan Function Token");
+	}
+}
