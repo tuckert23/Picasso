6bbd412fbd0c0e5648ef0b5258cef0e91daf0617
August Donovan <donovana22@mail.wlu.edu>
2020-11-07 22:14:33 -0500

    Initial implementation of CosToken.

---
+++ src/picasso/parser/tokens/functions/CosToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the tan function token
+ * @author August Donovan
+ *
+ */
+public class CosToken extends FunctionToken {
+
+	public CosToken() {
+		super("Cos Function Token");
+	}
+}
