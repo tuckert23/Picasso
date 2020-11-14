cda01f2940aaed3fbe6b40f7a2a5f1ba59ccbed8
Abdelrahman <aboeittaa23@mail.wlu.edu>
2020-11-13 22:56:38 -0500

    Evaluating a string from a file (working but still need modifications)

--- src/picasso/view/commands/FileEvaluator.java
+++ src/picasso/view/commands/FileEvaluator.java
@@ -1,9 +1,15 @@
 package picasso.view.commands;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import javax.swing.JFileChooser;
 
 import picasso.model.Pixmap;
+import picasso.parser.ExpressionTreeGenerator;
+import picasso.parser.language.ExpressionTreeNode;
 import picasso.util.FileCommand;
+import picasso.view.Frame;
 
 /**
  * @author Abdelrahman AboEitta
@@ -11,12 +17,29 @@ import picasso.util.FileCommand;
  */
 public class FileEvaluator extends FileCommand<Pixmap> {
 
+	String expression = "";
+	ExpressionTreeGenerator expTreeGen = new ExpressionTreeGenerator();
+	
 	public FileEvaluator() {
 		super(JFileChooser.OPEN_DIALOG);
 	}
 
 	@Override
 	public void execute(Pixmap target) {
+		String fileName = getFileName();
+		if (fileName != null) {
+			try {
+				File myFile = new File(fileName);
+				Scanner myReader = new Scanner(myFile);
+				while (myReader.hasNextLine()) {
+					expression = myReader.nextLine();
+				}
+				myReader.close();
+				Frame.expressionField.setText(expression);
+			} catch (FileNotFoundException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		}
 	}
-
 }
