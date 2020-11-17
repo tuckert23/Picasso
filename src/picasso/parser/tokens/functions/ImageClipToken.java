73fc4dd6492c1c2b3603dc68afb415818a021208
tuckert23 <tuckert23@mail.wlu.edu>
2020-11-17 14:01:04 -0500

    added imageClip token and added function to conf

---
+++ src/picasso/parser/tokens/functions/ImageClipToken.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * @author taylor
+ *
+ */
+public class ImageClipToken extends FunctionToken {
+
+
+	public ImageClipToken() {
+		super("ImageClip Function Token");
+	}
+
+}
