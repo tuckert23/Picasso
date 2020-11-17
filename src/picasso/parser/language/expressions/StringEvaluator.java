abbb552776b7129d0a63aaea5ad3ea17e86b2954
tuckert23 <tuckert23@mail.wlu.edu>
2020-11-17 13:54:26 -0500

    added ceiling token and some functionality for strings

--- src/picasso/parser/language/expressions/StringEvaluator.java
+++ src/picasso/parser/language/expressions/StringEvaluator.java
@@ -9,6 +9,7 @@ import picasso.parser.language.ExpressionTreeNode;
 
 /**
  * @author Abdelrahman AboEitta
+ * @author taylor
  *
  */
 public class StringEvaluator extends ExpressionTreeNode {
@@ -16,36 +17,79 @@ public class StringEvaluator extends ExpressionTreeNode {
 	private String myString;
 	public BufferedImage myImage;
 	private static final String DEFAULT_PATH = "images/";
-
+	private boolean image = false;
+	private int hash;
+	/*
+	private ArrayList fns = ["floor", "sin", "abs", "log", "rgbToYCrCB", "YCrCbtoRGB"
+	                         sin
+	                         abs
+	                         log
+	                         rgbToYCrCb
+	                         yCrCbtoRGB
+	                         perlinColor
+	                         perlinBW
+	                         atan
+	                         tan
+	                         cos
+	                         wrap
+	                         not
+	                         clamp
+	                         exp
+	                         imageWrap];
+	*/
+	
 	public StringEvaluator(String str) {
 		myString = str;
-		readImage(myString);
+		if (!readImage(myString))
+		{
+			readString(myString);
+		}
+			
 	}
 
 	@Override
 	public RGBColor evaluate(double x, double y) {
-		int xDomain = domainScaleToImage(x, myImage.getWidth());
-		int yDomain = domainScaleToImage(y, myImage.getHeight());
-		Color pixColor = new Color(myImage.getRGB(xDomain, yDomain));
-		return new RGBColor(pixColor);
+		if (image)
+		{
+			int xDomain = domainScaleToImage(x, myImage.getWidth());
+			int yDomain = domainScaleToImage(y, myImage.getHeight());
+			Color pixColor = new Color(myImage.getRGB(xDomain, yDomain));
+			return new RGBColor(pixColor);
+		}
+		else
+		{
+			return new RGBColor(new Color(hash));
+		}
 	}
 
 	public int domainScaleToImage(double value, int bound) {
 		return (int) (++value * bound) / 2;
 	}
 
-	private void readImage(String imageName) {
+	private boolean readImage(String imageName) {
 		String[] pathFolders = imageName.split("/");
+		boolean success = false;
 		if (pathFolders.length == 1) {
 			try {
 				myImage = ImageIO.read(new File(DEFAULT_PATH + imageName));
+				success = true;
+				image = true;
 			} catch (IOException e) {
 			}
 		} else {
 			try {
 				myImage = ImageIO.read(new File(imageName));
+				success = true;
+				image = true;
 			} catch (IOException e) {
 			}
 		}
+		return success;
+	}
+	
+	private void readString(String str)
+	{
+		hash = Math.abs(str.hashCode());
+		System.out.println(hash);
 	}
 }
---
+++ src/picasso/parser/tokens/functions/CeilingToken.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * @author taylor
+ *
+ */
+public class CeilingToken extends FunctionToken {
+
+	/**
+	 * @param name
+	 */
+	public CeilingToken() {
+		super("Ceiling Function Token");
+	}
+
+}
