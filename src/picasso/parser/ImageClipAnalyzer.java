639c9e42d087bfe6b371ee256bf419eee86df5b4
tuckert23 <tuckert23@mail.wlu.edu>
2020-11-17 14:04:21 -0500

    created imageClip and its analyzer – no implementation yet

---
+++ src/picasso/parser/ImageClipAnalyzer.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.tokens.Token;
+
+/**
+ * @author taylor
+ *
+ */
+public class ImageClipAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		return null;
+	}
+
+}
