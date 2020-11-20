003d954e55c52ccccea2ecec31626539d715584a
Will Medick <medickw21@mail.wlu.edu>
2020-11-19 20:23:59 -0500

    Got constructor from map

--- src/picasso/parser/RandomExpressionTreeGenerator.java
+++ src/picasso/parser/RandomExpressionTreeGenerator.java
@@ -20,64 +20,39 @@ import java.lang.Class;
  * @author Will Medick
  * 
  */
-//public class RandomExpressionTreeGenerator {
-//	
-//	final String unaryFunctionsPackage = "picasso.parser.language.expressions.unaryFunctions.";
-//	String[] unaryFunctionNames = { unaryFunctionsPackage + "Tan", unaryFunctionsPackage + "Sin" };
-//
-//	final String binaryFunctionsPackage = "picasso.parser.language.expressions.operators.";
-//	String[] binaryFunctionNames = { binaryFunctionsPackage + "Addition", binaryFunctionsPackage + "Minus" };
-//	
-//	final String operandsPackage = "picasso.parser.language.expressions.";
-//	String[] operandNames = { operandsPackage + "X", operandsPackage + "Y" };
-//
-//	String[] expressions = { unaryFunctionsPackage + "Tan", unaryFunctionsPackage + "Sin",
-//			binaryFunctionsPackage + "Addition", binaryFunctionsPackage + "Minus",
-//			operandsPackage + "X", operandsPackage + "Y"};
-//	
-//	ExpressionTreeNode[] params;
-//	
-//	private static Random rnd = new Random();
-//	Map<String, Constructor<?>> nameToConstructor = new HashMap<>();
-
 public class RandomExpressionTreeGenerator {
 	
-  private String[] constructorNames = {"Sin", "Tan", "Addition", "Minus", "X", "Y"};
-  private int[] constructorParams = {1, 1, 2, 2, 0, 0};
+	final String unaryFunctionsPackage = "picasso.parser.language.expressions.unaryFunctions.";
+	String[] unaryFunctionNames = { unaryFunctionsPackage + "Tan", unaryFunctionsPackage + "Sin" };
+
+	final String binaryFunctionsPackage = "picasso.parser.language.expressions.operators.";
+	String[] binaryFunctionNames = { binaryFunctionsPackage + "Addition", binaryFunctionsPackage + "Minus" };
+	
+	final String operandsPackage = "picasso.parser.language.expressions.";
+	String[] operandNames = { operandsPackage + "X", operandsPackage + "Y" };
+
+	String[] expressions = { unaryFunctionsPackage + "Tan", unaryFunctionsPackage + "Sin",
+			binaryFunctionsPackage + "Addition", binaryFunctionsPackage + "Minus",
+			operandsPackage + "X", operandsPackage + "Y"};
+//	
+	ExpressionTreeNode[] params;
+//	
+	private static Random rnd = new Random();
+	Map<String, Constructor<?>> nameToConstructor = new HashMap<>();
+
+//public class RandomExpressionTreeGenerator {
+//	
+//  private String[] constructorNames = {"Sin", "Tan", "Addition", "Minus", "X", "Y"};
+//  private int[] constructorParams = {1, 1, 2, 2, 0, 0};
   //private Map<String, Integer> constructorParams = new HashMap<String, Integer>();
   //constructorParams.put("Sin", new Integer(1));
   //constructorParams.put("Tan", new Integer(1));
   
   
   // private Map<String, int> constructorParams = {"Sin", 1, "Tan", 1, "Addition", 2, "Minus", 2, "X", 0, "Y", 0};
-  private int lenConstNames = constructorNames.length;
-  private Random rand = new Random();
+//  private int lenConstNames = constructorNames.length;
+//  private Random rand = new Random();
  
-  private ExpressionTreeNode makeOneExpression()
-  {
-	int ind = rand.nextInt(lenConstNames);
-    String randConstructor = constructorNames[ind];
-    int numParams = constructorParams[ind];
-    
- 
- 
-    if (numParams == 0)
-    {
-      return (ExpressionTreeNode) Class.forName(randConstructor).newInstance();
-    }
-    else if (numParams == 1)
-    {
-      return (ExpressionTreeNode) Class.forName(randConstructor).newInstance(makeOneExpression());
-    }
-    else if (numParams == 2)
-    {
-      return (ExpressionTreeNode) Class.forName(randConstructor).newInstance(makeOneExpression(), makeOneExpression());
-    }
- 
-  }
-}
-
-	
 	public RandomExpressionTreeGenerator() {
 		for (String unaryClassName : unaryFunctionNames) {
 			try {
@@ -134,43 +109,60 @@ public class RandomExpressionTreeGenerator {
 				}
 			}
 		}
+  
+  
+  
+//	private ExpressionTreeNode makeOneExpression() {
+//		int ind = rand.nextInt(lenConstNames);
+//	    String randConstructor = constructorNames[ind];
+//	    int numParams = constructorParams[ind];
+//	 
+//	    if (numParams == 0)
+//	    {
+//	      return (ExpressionTreeNode) Class.forName(randConstructor).newInstance();
+//	    }
+//	    else if (numParams == 1)
+//	    {
+//	      return (ExpressionTreeNode) Class.forName(randConstructor).newInstance(makeOneExpression());
+//	    }
+//	    else if (numParams == 2)
+//	    {
+//	      return (ExpressionTreeNode) Class.forName(randConstructor).newInstance(makeOneExpression(), makeOneExpression());
+//	    }
+// 
+//  }
+//}
 
-//	public ExpressionTreeNode makeOneExpression() {
-//			String classString = expressions[rnd.nextInt(expressions.length)];
-//			Constructor<?> constructor = nameToConstructor.values();
-//			params = new ExpressionTreeNode[numParams];
-//			numParams = 
-//			
-//			if (numParams == 0)
-//			{
-//				return null;
-//			}
-//
-//			if (numParams == 1) {
-//				params[0] = makeOneExpression();
-//				return params[0];
-//			}
-//			
-//			if (numParams == 2) {
-//				return params[0];
-//			}
-//			
-//			for (int i = 0; i < numParams; i++) {
-//					System.err.println("This Node: ");
-//					params[i] = new makeOneExpression();
-//			}
-//
-//			try {
-//				ExpressionTreeNode etn = (ExpressionTreeNode) constructor.newInstance(params);
-//				System.err.print("Node: ");
-//				System.err.println(etn);
-//				return etn;
-//			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-//					| InvocationTargetException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		return null;
-//	}
+	public ExpressionTreeNode makeOneExpression() {
+			String classString = expressions[rnd.nextInt(expressions.length)];
+			Constructor<?> constructor = nameToConstructor.get(classString);
+			int numParams = constructor.getParameterCount();
+			params = new ExpressionTreeNode[numParams];
+			
+			for (int i = 0; i < numParams; i++) {
+					System.out.println("This Node: ");
+					params[i] = makeOneExpression();
+			}
+
+			try {
+				ExpressionTreeNode etn = (ExpressionTreeNode) constructor.newInstance(params);
+				System.err.print("Node: ");
+				System.err.println(etn);
+				return etn;
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException e) {
+				e.printStackTrace();
+			}
+			ExpressionTreeNode etn;
+			try {
+				etn = (ExpressionTreeNode) constructor.newInstance();
+				return etn;
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+	}
 		
 }
